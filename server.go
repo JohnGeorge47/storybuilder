443351f8bcc8367f5904f6c7712b3310385ecf28
John George <john@exotel.in>
2019-09-30 23:50:43 +0530

    refactoring

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/JohnGeorge47/storybuilder/jsonparse"
+	"github.com/JohnGeorge47/storybuilder/storyhandlers"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +17,11 @@ func main() {
 	story := jsonparse.OpenJSONFile()
 	fmt.Println(story["intro"].Title)
 	http.HandleFunc("/", handler)
-	// http.HandleFunc("/edit", editHandler)
+	http.HandleFunc("/intro", storyhandlers.New(story, "intro").StoryBuilder())
+	http.HandleFunc("/new-york", storyhandlers.New(story, "new-york").StoryBuilder())
+	http.HandleFunc("/denver", storyhandlers.New(story, "denver").StoryBuilder())
+	http.HandleFunc("/home", storyhandlers.New(story, "home").StoryBuilder())
+	http.HandleFunc("/debate", storyhandlers.New(story, "debate").StoryBuilder())
 	fmt.Println("Starting the server on port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
--- storyhandlers/storyhandlers.go
+++ storyhandlers/storyhandlers.go
@@ -1,14 +1,45 @@
 package storyhandlers
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
+	"strings"
 
 	"github.com/JohnGeorge47/storybuilder/jsonparse"
 )
 
-func Introhandler(story map[string]jsonparse.StoryChapterStruct) http.HandlerFunc {
-	fn := func(w http.ResponseWriter, r *http.Request) {
+type Info struct {
+	Title   string
+	Story   []string
+	Options []jsonparse.Arcs
+}
 
+type Gopher struct {
+	Story map[string]jsonparse.StoryChapterStruct
+	Phase string
+}
+
+func New(story map[string]jsonparse.StoryChapterStruct, phase string) *Gopher {
+	return &Gopher{
+		Story: story,
+		Phase: phase,
+	}
+}
+
+//To create the story of the gopher
+func (stor *Gopher) StoryBuilder() http.HandlerFunc {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		var part string
+		if r.URL.String() == "/" {
+			part = "intro"
+		} else {
+			part = strings.TrimPrefix(stor.Phase, "/")
+		}
+		fmt.Println(part)
+		data := Info{Title: stor.Story[part].Title, Story: stor.Story[part].Story, Options: stor.Story[part].Options}
+		t, _ := template.ParseFiles("./views/intro.html")
+		t.Execute(w, data)
 	}
 	return http.HandlerFunc(fn)
 }
